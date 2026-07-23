57403ab4f57bde41377dca5f9c1e2c92946c473d
ashish bongu <bonguashish532222@gmail.com>
2026-07-23 14:11:52 +0530

    updated landing page ui

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,26 +2,63 @@ import heroVideo from "../assets/hero.mp4";
 
 export default function Hero() {
   return (
-    <section className="relative h-screen bg-black overflow-hidden">
+    <section className="relative min-h-screen bg-black overflow-hidden">
 
-      {/* Video fixed at bottom of hero */}
-      <div className="absolute bottom-0 left-1/2 -translate-x-1/2">
-        <div className="w-[300px] md:w-[500px] overflow-hidden">
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline
+      <div className="max-w-7xl mx-auto h-screen px-6 lg:px-12 flex flex-col-reverse lg:flex-row items-center">
+
+        {/* LEFT */}
+        <div className="flex-1 text-center lg:text-left z-10 pb-10 lg:pb-0">
+
+          <h1 className="text-white font-black tracking-tight
+                         text-5xl sm:text-4xl md:text-5xl lg:text-6xl">
+            B ASHISH
+          </h1>
+
+          <p className="mt-5 text-neutral-400 max-w-xl text-base sm:text-lg">
+            Full Stack Developer • AI/ML Enthusiast • Content Creator
+            <br />
+            Student at Indian Institute of Information Technology, Dharwad
+          </p>
+
+          <button
             className="
-              w-full
-              h-auto
-              pointer-events-none
-              select-none
-            "
+            mt-6
+            px-8
+            py-4
+            rounded-full
+            bg-black
+            text-white
+            border
+            font-semibold
+            hover:text-black
+            hover:bg-white
+            transition
+            duration-300"
           >
-            <source src={heroVideo} type="video/mp4" />
-          </video>
+            Know My Story
+          </button>
+
         </div>
+
+        {/* RIGHT */}
+        <div className="flex-1 flex justify-center lg:justify-end items-end h-full">
+
+          <div className="w-[280px] sm:w-[340px] md:w-[420px] lg:w-[520px]">
+
+            <video
+              autoPlay
+              loop
+              muted
+              playsInline
+              className="w-full h-auto pointer-events-none select-none"
+            >
+              <source src={heroVideo} type="video/mp4" />
+            </video>
+
+          </div>
+
+        </div>
+
       </div>
 
     </section>
