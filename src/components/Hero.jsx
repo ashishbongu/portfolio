c7753c469335980c8b1a3a159145f2d770cb0026
ashish bongu <bonguashish532222@gmail.com>
2026-08-21 11:34:13 +0530

    updated responsiveness for smaller screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ export default function Hero() {
         <div className="z-10 flex w-full flex-1 flex-col justify-center pb-4 pt-24 text-center lg:w-auto lg:justify-center lg:pb-0 lg:pt-0 lg:text-left">
 
           <h1 className="instrument-serif text-white font-normal tracking-wide
-                         text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+                         text-5xl sm:text-5xl md:text-6xl lg:text-7xl">
             B <span className="italic text-[#4D6CFA]">ASHISH</span>
           </h1>
 
@@ -56,9 +56,9 @@ export default function Hero() {
         </div>
 
         {/* RIGHT */}
-        <div className="flex w-full min-h-0 flex-1 items-end justify-end lg:h-full lg:flex-1">
+        <div className="flex w-full min-h-0 flex-1 items-center justify-center lg:h-full lg:flex-1 lg:items-end lg:justify-end">
 
-          <div className="w-[min(110vw,520px)] max-w-none translate-x-6 sm:w-[min(100vw,520px)] lg:w-[520px] lg:translate-x-0">
+          <div className="w-[min(125vw,600px)] max-w-none -translate-y-4 sm:w-[min(115vw,560px)] lg:w-[520px] lg:translate-x-0 lg:translate-y-0">
 
             <video
               autoPlay
